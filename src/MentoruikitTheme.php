10c889059b8d4f588b2764896f95ca40c3a980b6
jzachary <jamie.zachary@gmail.com>
2019-02-06 08:59:06 -0600

    Add files via upload

---
+++ src/MentoruikitTheme.php
@@ -0,0 +1,8 @@
+<?php namespace Crt\MentoruikitTheme;
+
+use Anomaly\Streams\Platform\Addon\Theme\Theme;
+
+class MentoruikitTheme extends Theme
+{
+
+}
---
+++ tests/Feature/MentoruikitThemeTest.php
@@ -0,0 +1,10 @@
+<?php namespace Crt\MentoruikitTheme\Test\Feature;
+
+class MentoruikitThemeTest extends \TestCase
+{
+
+    public function testFeature()
+    {
+        $this->markTestSkipped('Not implemented.');
+    }
+}
